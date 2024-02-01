6cf2a51347c9918be99a81337260629547b912ef
Niels de Hoog <njdehoog@gmail.com>
2024-02-01 16:31:06 +0000

    Create basic cell styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
+    spacing: {
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
+      5: '20px',
+      6: '40px',
+    },
     extend: {},
   },
   plugins: [],
