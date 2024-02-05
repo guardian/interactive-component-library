ce1852ae30c3acfcf75af5b7c3277bdc02e5ad28
Niels de Hoog <njdehoog@gmail.com>
2024-02-05 15:58:31 +0000

    Create import aliases

--- .storybook/vite.config.ts
+++ .storybook/vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vitest/config'
 import tailwindcss from 'tailwindcss'
 import { UserConfigExport } from 'vite'
 import preact from '@preact/preset-vite'
+import path from 'node:path'
 
 const app = async (): Promise<UserConfigExport> => {
   return defineConfig({
+    resolve: {
+      alias: {
+        '$atoms': path.resolve(__dirname, '../src/lib/components/atoms'),
+        '$molecules': path.resolve(__dirname, '../src/lib/components/molecules'),
+        '$headless': path.resolve(__dirname, '../src/lib/components/headless'),
+      },
+    },
     plugins: [preact()],
     css: {
       postcss: {
