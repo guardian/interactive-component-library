8591d06c2fda0a5aae488f2152dd0cd0386302bd
Niels de Hoog <njdehoog@gmail.com>
2024-02-06 15:47:24 +0000

    Updates to table

--- src/lib/components/atoms/legend-item/index.jsx
+++ src/lib/components/atoms/legend-item/index.jsx
@@ -1,8 +1,8 @@
 export const LegendItem = ({ circleClass, text }) => {
   return (
     <div className="flex items-center">
-      <span className={`w-[11px] h-[11px] rounded-full mr-1 ${circleClass}`}></span>
-      <p className="text-neutral-7">{text}</p>
+      <span className={`w-[11px] h-[11px] rounded-full mr-1 shrink-0 ${circleClass}`}></span>
+      <p className="text-neutral-7 shrink">{text}</p>
     </div>
   )
 }
