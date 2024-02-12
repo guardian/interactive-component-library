946497a2397e949bc08363e5dafe32b24b0778c9
Niels de Hoog <njdehoog@gmail.com>
2024-02-12 08:19:38 +0000

    Adjust tooltip y position when needed

--- src/lib/components/molecules/tooltip/index.jsx
+++ src/lib/components/molecules/tooltip/index.jsx
@@ -30,9 +30,13 @@ export function Tooltip({ for: elementRef, renderIn: refOrSelector, children }) 
     }
 
     const tooltipWidth = tooltipRef.current.offsetWidth
+    const tooltipHeight = tooltipRef.current.offsetHeight
     const displayElementRect = displayElement.getBoundingClientRect()
     if (newPosition.x + tooltipWidth > displayElementRect.right) {
       newPosition.x -= tooltipWidth
+    }
+    if (newPosition.y + tooltipHeight > displayElementRect.bottom) {
+      newPosition.y -= tooltipHeight
     }
 
     setTooltipPosition(newPosition)
@@ -48,50 +52,3 @@ export function Tooltip({ for: elementRef, renderIn: refOrSelector, children }) 
     displayElement,
   )
 }
-
-// export const Tooltip = React.memo(
-//   ({ children, width, bboxTop, containerRef, containerBounds, innerWidth, innerHeight }) => {
-//     const ttRef = useRef(null)
-//     const [bbTop] = useState(containerBounds.top)
-
-//     const ttBox = ttRef.current && ttRef.current.getBoundingClientRect()
-//     const ttWidth = width || (ttRef.current && ttBox.width)
-//     const ttHeight = ttRef.current && ttBox.height
-
-//     const useMousePosition = () => {
-//       const [eCoords, setECoords] = useState({ clientX: 0, clientY: 0 })
-
-//       const eventHandler = useCallback(
-//         ({ clientX, clientY }) => {
-//           requestAnimationFrame(() => setECoords({ clientX, clientY }))
-//         },
-//         [setECoords],
-//       )
-
-//       useEffect(() => {
-//         containerRef.current.addEventListener('mousemove', eventHandler)
-
-//         return () => {
-//           containerRef.current.removeEventListener('mousemove', eventHandler)
-//         }
-//       }, [containerRef])
-//       return eCoords
-//     }
-
-//     const { clientX, clientY } = useMousePosition()
-//     const offsetX = clientX - ttWidth / 2 < 0 ? clientX - ttWidth / 2 : 0
-//     const offsetY = clientY + ttHeight > innerHeight ? -ttHeight - 15 : 25
-//     const left = clientX - containerBounds.left - ttWidth / 2 - offsetX
-//     const top = innerWidth < 980 ? clientY - bbTop + 36 + offsetY : clientY - bboxTop + offsetY
-
-//     return (
-//       <div
-//         class="gv-tooltip"
-//         ref={ttRef}
-//         style={innerWidth < 740 ? null : { position: 'absolute', left, top, width: `${ttWidth}px` }}
-//       >
-//         {children}
-//       </div>
-//     )
-//   },
-// )
