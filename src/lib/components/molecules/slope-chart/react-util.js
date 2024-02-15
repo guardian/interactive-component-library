2122b86264dee13493a03492dac3d41146495363
Antonio Voce <antonio.voce@guardian.co.uk>
2024-02-15 14:39:54 +0000

    check window

--- src/lib/components/molecules/slope-chart/react-util.js
+++ src/lib/components/molecules/slope-chart/react-util.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react'
 
 function useWindowSize() {
+  if (typeof window === 'undefined') {
+    return { width: 0, height: 0 }
+  }
+
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
