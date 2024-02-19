3e9724ed720b38846cfd2667a328b9a44fefc16f
Antonio Voce <antonio.voce@guardian.co.uk>
2024-02-19 17:41:10 +0000

    remove external dependencies

--- src/lib/components/particles/map-locator/index.jsx
+++ src/lib/components/particles/map-locator/index.jsx
@@ -1,37 +1,26 @@
-import proj4d3 from 'proj4d3'
-import { geoPath } from 'd3-geo'
-
-export const MapLocator = ({ proj4String, featureCollection, markerCoordinates, width, height, markerColor, backgroundColor='#707070' }) => {
-  const proj = proj4d3(proj4String).fitSize([width, height], featureCollection)
-  const path = geoPath().projection(proj)
-  const projectedCoordinates = proj(markerCoordinates)
-
-  return (
-    <svg
-      viewBox={`0 0 ${width} ${height}`}
-      preserveAspectRatio={"xMinYMin"}>
-      <g>
-        {
-          featureCollection.features.map((f, i) => {
-            return (
-              <path
-                fill={backgroundColor}
-                key={i}
-                d={path(f)}
-                id={f.id}
-              />
-            )
-          })
-        }
-      </g>
-      <g>
-        <circle
-          cx={projectedCoordinates[0]}
-          cy={projectedCoordinates[1]}
-          r={15}
-          fill={markerColor}
-        />
-      </g>
-    </svg>
-  )
-}
+export const MapLocator = ({ path, projection, features, markerCoordinates, width, height, markerColor, backgroundColor='#707070', markerRadius=15 }) =>
+  <svg
+    viewBox={`0 0 ${width} ${height}`}
+    preserveAspectRatio={"xMinYMin"}>
+    <g>
+      {
+        features.map((f, i) => {
+          return (
+            <path
+              fill={backgroundColor}
+              key={i}
+              d={path(f)}
+            />
+          )
+        })
+      }
+    </g>
+    <g>
+      <circle
+        cx={projection(markerCoordinates)[0]}
+        cy={projection(markerCoordinates)[1]}
+        r={markerRadius}
+        fill={markerColor}
+      />
+    </g>
+  </svg>
--- src/lib/components/particles/map-locator/maplocator.stories.jsx
+++ src/lib/components/particles/map-locator/maplocator.stories.jsx
@@ -1,6 +1,8 @@
 import { MapLocator } from '.'
 import ukmap from './ukmap.json'
 import { feature } from 'topojson-client'
+import proj4d3 from 'proj4d3'
+import { geoPath } from 'd3-geo'
 
 const meta = {
   title: 'Particles/MapLocator',
@@ -11,17 +13,23 @@ const featureCollection = {
   type: 'FeatureCollection',
   features: feature(ukmap, ukmap.objects.ukmap).features
 }
+const width = 200
+const height = 300
+const proj4String = '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs'
+
+const proj = proj4d3(proj4String).fitSize([width, height], featureCollection)
+const path = geoPath().projection(proj)
 
 export default meta
 
 export const Default = {
   args: {
-    proj4String: '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs',
-    featureCollection,
+    projection: proj,
+    path,
+    features: featureCollection.features,
     markerCoordinates: [0.1276, 51.5072],
-    width: 200,
-    height: 300,
     markerColor: '#c70000',
+    markerRadius: 15,
     backgroundColor: '#707070'
   },
   render: (args) => <MapLocator {...args} />
