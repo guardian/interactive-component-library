40e6a1cbecec2095341b9ada23abb35504b4532b
Antonio Voce <antonio.voce@guardian.co.uk>
2024-02-23 15:33:31 +0000

    waffle wip

--- src/lib/components/particles/waffle/index.jsx
+++ src/lib/components/particles/waffle/index.jsx
@@ -1,26 +1,32 @@
-const renderUnit = ({ squares, attributes }) => squares ? <rect {...attributes} /> : <circle {...attributes} />
+const renderUnit = (squares, attributes) => squares ? <rect {...attributes} /> : <circle {...attributes} />
 
-const Waffle = ({ seats, rows, total, squares=false }) => {
-  const width = 800
-  const partySeats = seats ? [].concat.apply([], seats.filter(d => d.total > 0).map(partyObj => Array(partyObj.total).fill(partyObj.party))) : Array(totalSeats).fill(null)
-
+export const Waffle = ({ units, rows, total, squares = false, abbreviationAccessor = 'party', onMouseOver, paddingTop }) => {
+  //units is an array of objects with the following structure: { [abbreviationAccessor]: 'A' }.
+  const width = 1300
   const columns = Math.ceil(total / rows)
-  const unitWidth = Math.floor(width / columns)
+  const unitWidth = width / columns
   const unitHeight = unitWidth
-
-  const height = rows * unitHeight
-
+  const height = rows * unitHeight + paddingTop
+  
   return (
     <div class='gv-waffle'>
-      <svg viewBox={`0 0 ${width} ${height}`} preserveAspectRatio={"xMinYMin"}>
-        {
-          partySeats.map((party, j) =>
-            renderUnit(squares, { class: `fill-color--${party}`, height: unitHeight - 0.8, width: unitWidth - 0.8, x: unitWidth * Math.floor(j / rows), y: unitHeight * (j % rows)})
-          )
-        }
+      <svg viewBox={`0 0 ${width} ${height}`}>
+        <g>
+          {
+            units.map((unit, j) => {
+              const attributes = squares ?
+              { onMouseOver: e => onMouseOver(unit, e), class: `fill-color--${unit[abbreviationAccessor]}`, height: unitHeight, width: unitWidth, x: unitWidth * Math.floor(j / rows), y: unitHeight * (j % rows) + paddingTop} :
+              { onMouseOver: e => onMouseOver(unit, e), class: `fill-color--${unit[abbreviationAccessor]}`, r: unitWidth / 2, transform: `translate(${unitWidth * Math.floor(j / rows) + unitWidth / 2}, ${unitHeight * (j % rows) + unitHeight / 2 + paddingTop})` }
+
+              return renderUnit(squares, attributes)
+            })
+          }
+          <g>
+            <line x1={width / 2} x2={width / 2} y1={20} y2={height} stroke='red' />
+            <text x={width / 2} y={0} style={{ textAnchor: 'middle', alignmentBaseline: 'hanging'}} fontSize='20'>{`${Math.ceil(total / 2)} for a majority`}</text>
+          </g>
+        </g>
       </svg>
     </div>
   )
 }
-
-  export default Waffle
---
+++ src/lib/components/particles/waffle/waffle.stories.jsx
@@ -0,0 +1,39 @@
+import { Waffle } from '.'
+
+const meta = {
+  title: 'Particles/Waffle',
+  component: Waffle,
+}
+export default meta
+
+// this should also be the sort order, from bigger party to smaller with undeclared at the end
+const summary = {
+  lab: 350,
+  con: 200,
+  snp: 50,
+  ld: 30,
+  green: 1,
+  undeclared: 19
+}
+
+
+const partySeats = Object.keys(summary).map(party => Array(summary[party]).fill({ party })).flat()
+const args = {
+  units: partySeats,
+  rows: 5,
+  total: 650, 
+  abbreviationAccessor:'party',
+  paddingTop: 30,
+  onMouseOver: (a, b) => console.log(a, b)
+}
+
+export const Default = {
+  args,
+  render: (args) => <Waffle {...args} squares={false} />,
+}
+
+export const Squares = {
+  args,
+  render: (args) => <Waffle {...args} squares={true} />,
+}
+
