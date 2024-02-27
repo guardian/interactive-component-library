95b6174e63517b0b2257831281e3f69022886bf7
Niels de Hoog <njdehoog@gmail.com>
2024-02-27 10:16:35 +0000

    Style slope chart

--- src/lib/components/molecules/slope-chart/index.jsx
+++ src/lib/components/molecules/slope-chart/index.jsx
@@ -8,6 +8,7 @@ export const SlopeChart = ({
   id,
   slopeChartItems,
   padding = { left: 20, right: 24, top: 20, bottom: 16 },
+  spacing = 8,
   domain,
   previousValueKey,
   nextValueKey,
@@ -19,10 +20,14 @@ export const SlopeChart = ({
   const wrapperRef = useRef(null)
   const [width, setWidth] = useState(0)
   const windowSize = useWindowSize()
-  const contentSize = Math.floor(width - padding.left - padding.right)
-  const height = contentSize + padding.top + padding.bottom
-  const yScale = scaleLinear(domain, [contentSize, 0])
+
+  const contentWidth = Math.floor(width - padding.left - padding.right)
+
+  const height = contentWidth + padding.top + padding.bottom
+  const yScale = scaleLinear(domain, [contentWidth, 0])
   const show = width > 0
+
+  const contentHeight = Math.floor(height - padding.top - padding.bottom)
 
   useLayoutEffect(() => {
     const newWidth = wrapperRef.current.getBoundingClientRect().width
@@ -30,8 +35,9 @@ export const SlopeChart = ({
   }, [windowSize])
 
   const labelLeftPositions = useMemo(
-    () => positionLabels(slopeChartItems, previousValueKey, yScale, abbreviationKey, padding.left - 8, padding.top),
-    [slopeChartItems, previousValueKey, padding, abbreviationKey, yScale],
+    () =>
+      positionLabels(slopeChartItems, previousValueKey, yScale, abbreviationKey, padding.left - spacing, padding.top),
+    [slopeChartItems, previousValueKey, padding, abbreviationKey, yScale, spacing],
   )
   const labelRightPositions = useMemo(
     () =>
@@ -40,10 +46,10 @@ export const SlopeChart = ({
         nextValueKey,
         yScale,
         abbreviationKey,
-        padding.left + contentSize + 8,
+        padding.left + contentWidth + 8,
         padding.top,
       ),
-    [slopeChartItems, abbreviationKey, padding, yScale, nextValueKey, contentSize],
+    [slopeChartItems, abbreviationKey, padding, yScale, nextValueKey, contentWidth],
   )
 
   styles = mergeStyles(defaultStyles, styles)
@@ -52,36 +58,28 @@ export const SlopeChart = ({
     <div ref={wrapperRef}>
       {show && (
         <svg id={id} width={width} height={height}>
-          {slopeChartItems.map((item) => {
-            const itemStyles = mergeStyles({ ...styles }, item.styles)
-            return (
-              <g key={item[abbreviationKey]}>
-                <line
-                  x1={padding.left}
-                  y1={yScale(item[previousValueKey]) + padding.top}
-                  x2={padding.left + contentSize}
-                  y2={yScale(item[nextValueKey]) + padding.top}
-                  className={itemStyles.line}
-                />
-                <circle
-                  cx={padding.left}
-                  cy={yScale(item[previousValueKey]) + padding.top}
-                  r={4}
-                  className={itemStyles.circle}
-                />
-                <circle
-                  cx={padding.left + contentSize}
-                  cy={yScale(item[nextValueKey]) + padding.top}
-                  r={4}
-                  className={itemStyles.circle}
-                />
-              </g>
-            )
-          })}
+          <g transform={`translate(${padding.left} ${padding.top})`}>
+            {slopeChartItems.map((item) => {
+              const itemStyles = mergeStyles({ ...styles }, item.styles)
+              return (
+                <g key={item[abbreviationKey]}>
+                  <line
+                    x1={0}
+                    y1={yScale(item[previousValueKey])}
+                    x2={contentWidth}
+                    y2={yScale(item[nextValueKey])}
+                    className={itemStyles.line}
+                  />
+                  <circle cx={0} cy={yScale(item[previousValueKey])} r={4} className={itemStyles.circle} />
+                  <circle cx={contentWidth} cy={yScale(item[nextValueKey])} r={4} className={itemStyles.circle} />
+                </g>
+              )
+            })}
+          </g>
           {labelLeftPositions.map((item) => {
             return (
               <g key={`${item.key}-left`}>
-                <text x={item.x} y={item.y} textAnchor="end" class="gv-slope-chart-text">
+                <text x={item.x} y={item.y} className={styles.label} text-anchor="end">
                   {item.value}
                 </text>
               </g>
@@ -90,18 +88,28 @@ export const SlopeChart = ({
           {labelRightPositions.map((item) => {
             return (
               <g key={`${item.key}-right`}>
-                <text x={item.x} y={item.y} class="gv-slope-chart-text">
-                  {item.value} {item.key}
+                <text x={item.x} y={item.y} className={styles.label}>
+                  <tspan>{item.value}</tspan>
+                  <tspan dx="4" className={styles.category}>
+                    {item.key}
+                  </tspan>
                 </text>
               </g>
             )
           })}
           {
-            <g>
-              <text x={padding.left} y={height}>
+            <g transform={`translate(${padding.left} ${padding.top})`}>
+              <line x1={0} x2={contentWidth} y1={contentHeight} y2={contentHeight} className={styles.axis} />
+              <text x={0} y={contentHeight + spacing / 2} dominant-baseline="hanging" className={styles.label}>
                 {previousLabel}
               </text>
-              <text x={width - padding.right} y={height} textAnchor="end">
+              <text
+                x={contentWidth}
+                y={contentHeight + spacing / 2}
+                dominant-baseline="hanging"
+                className={styles.label}
+                text-anchor="end"
+              >
                 {nextLabel}
               </text>
             </g>
