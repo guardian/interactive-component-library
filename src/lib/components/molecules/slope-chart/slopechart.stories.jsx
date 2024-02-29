4a7f33761d93efd72fde3a4a2f69be961da473ba
Niels de Hoog <njdehoog@gmail.com>
2024-02-29 16:00:36 +0000

    Refactor slope chart using new styling method
    Finish slope chart refactor

--- src/lib/components/molecules/slope-chart/slopechart.stories.jsx
+++ src/lib/components/molecules/slope-chart/slopechart.stories.jsx
@@ -7,88 +7,35 @@ const meta = {
 
 export default meta
 
-const data = [
-  {
-    party: 'Con',
-    elected: 20,
-    overallSeats: 20,
-    change: 6,
-    percentageSeats: '60.606',
-    losses: false,
-    hasSeats: true,
-    prevSeats: 14,
-    marginLeft: 50,
-    width: 11.1,
-    styles: {
-      line: 'stroke-color--con',
-      circle: 'fill-color--con',
-    },
-  },
-  {
-    party: 'Lab',
-    elected: 4,
-    overallSeats: 4,
-    change: 2,
-    percentageSeats: '12.121',
-    losses: false,
-    hasSeats: true,
-    prevSeats: 2,
-    marginLeft: 50,
-    width: 3.7,
-    styles: {
-      line: 'stroke-color--lab',
-      circle: 'fill-color--lab',
-    },
-  },
-  {
-    party: 'LibDem',
-    elected: 2,
-    overallSeats: 2,
-    change: -1,
-    percentageSeats: '6.061',
-    losses: true,
-    hasSeats: true,
-    prevSeats: 3,
-    marginLeft: 48.1,
-    width: 1.9,
-    styles: {
-      line: 'stroke-color--libdem',
-      circle: 'fill-color--libdem',
-    },
-  },
-  {
-    party: 'Green',
-    elected: 1,
-    overallSeats: 1,
-    change: 0,
-    percentageSeats: '3.030',
-    losses: false,
-    hasSeats: true,
-    prevSeats: 1,
-    marginLeft: 50,
-    width: 0,
-    styles: {
-      line: 'stroke-color--green',
-      circle: 'fill-color--green',
-    },
-  },
-]
-
-const totalSeats = 40
-
-const args = {
-  id: 'slope-chart',
-  slopeChartItems: data,
-  padding: { left: 20, right: 60, top: 20, bottom: 20 },
-  domain: [0, totalSeats],
-  previousValueKey: 'prevSeats',
-  nextValueKey: 'overallSeats',
-  abbreviationKey: 'party',
-  previousLabel: '2019',
-  nextLabel: '2024',
-}
-
 export const Default = {
-  args,
+  args: {
+    id: 'slope-chart',
+    domain: [0, 100],
+    lines: [
+      {
+        y1: 30,
+        y2: 10,
+        styles: {
+          line: 'stroke-color--snp',
+          circle: 'fill-color--snp',
+        },
+      },
+      {
+        y1: 20,
+        y2: 70,
+        color: 'LD',
+        styles: {
+          line: 'stroke-color--libdem',
+          circle: 'fill-color--libdem',
+        },
+      },
+    ],
+    y2Label: (d) => `${d.y2}%`,
+    axis: {
+      startLabel: '2019',
+      endLabel: '2024',
+    },
+    padding: { left: 24, right: 36, top: 20, bottom: 20 },
+  },
   render: (args) => <SlopeChart {...args} />,
 }
