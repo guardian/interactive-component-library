c7772acf24110b48074bc4d9436d519ebbf3e98a
Niels de Hoog <njdehoog@gmail.com>
2024-02-29 10:30:24 +0000

    Add story to explain overflow behaviour

--- src/lib/components/molecules/tooltip/tooltip.stories.jsx
+++ src/lib/components/molecules/tooltip/tooltip.stories.jsx
@@ -29,3 +29,36 @@ function TooltipPreview() {
 export const Default = {
   render: () => <TooltipPreview />,
 }
+
+function TooltipOverflow() {
+  const tooltipped = useRef(null)
+
+  return (
+    <>
+      <div style="width: 100%; padding: 20px; background-color: #EAF2F8; font-family: var(--text-sans); border: 1px solid #CCC;">
+        <p style={{ marginBottom: 10 }}>Tooltip is rendered in this element</p>
+        <div ref={tooltipped} style="width: 100%; height: 300px; padding: 20px; background-color: #2980B9;">
+          <p>
+            Tooltip will show outside of tooltipped element, but never outside the bounds of the element it's rendered
+            in.
+          </p>
+        </div>
+      </div>
+
+      <Tooltip for={tooltipped} renderIn="#storybook-root">
+        {({ x, y }) => (
+          <div style="border: 1px solid #333; background-color: #FFF; padding: 10px;">
+            <p>Tooltip position</p>
+            <p>
+              x: {x}, y: {y}
+            </p>
+          </div>
+        )}
+      </Tooltip>
+    </>
+  )
+}
+
+export const Overflow = {
+  render: () => <TooltipOverflow />,
+}
