5ca2d0f3cd12fec93f68ba0004b68564ca81bc78
Anna <ajwl@users.noreply.github.com>
2024-03-04 13:49:40 +0000

    Relative time sentence + control change + circle icon: basic implementation (#19)
    * Ticker Item: basic implementation
    
    * cleanedup: gradient + circle+controlchange
    
    * control change+ time sentence cleanup
    
    * dayjs - import plugin normally

---
+++ src/lib/components/particles/relative-time-sentence/relative-time-sentence.stories.jsx
@@ -0,0 +1,14 @@
+import {RelativeTimeSentence} from "."
+
+export default {
+    title: 'Particles/RelativeTimeSentence',
+    component: RelativeTimeSentence
+}
+
+
+
+export const Default = {
+    args: {
+        timeStamp: Date.parse("2024-01-22T00:00:00.000Z")
+    }
+}
