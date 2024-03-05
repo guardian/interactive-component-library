dc1993d0c67be7e038887247146825dc70fccb9f
Niels de Hoog <njdehoog@gmail.com>
2024-03-05 11:25:35 +0000

    Create PageSection component

---
+++ src/lib/components/molecules/page-section/index.jsx
@@ -0,0 +1,13 @@
+import defaultStyles from './style.module.scss'
+import { mergeStyles } from '$styles/helpers/mergeStyles'
+
+export function PageSection({ children, styles, borderTop = false }) {
+  styles = mergeStyles(defaultStyles, styles)
+
+  return (
+    <section className={[styles.section, borderTop && styles.borderTop].join(' ')}>
+      <div className={styles.header}>{children.header}</div>
+      <div className={styles.content}>{children.content}</div>
+    </section>
+  )
+}
