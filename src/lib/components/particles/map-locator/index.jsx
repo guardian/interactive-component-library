484d638a3d43259a7f120461562eafa461462a78
Antonio Voce <antonio.voce@guardian.co.uk>
2024-03-07 15:03:43 +0000

    clean up

--- src/lib/components/particles/map-locator/index.jsx
+++ src/lib/components/particles/map-locator/index.jsx
@@ -1,19 +1,20 @@
 import defaultStyles from "./style.module.css"
 import { mergeStyles } from "$styles/helpers/mergeStyles"
 
-export const MapLocator = ({ svgPath, size, marker, styles }) => {
+export const MapLocator = ({ basemapImage, size, marker, styles, projection }) => {
   styles = mergeStyles({ ...defaultStyles }, styles)
-
+  const projectedCoords = projection(marker.coordinates)
+  
   return (
     <svg
       class={styles.svg}
       viewBox={`0 0 ${size.width} ${size.height}`}
       preserveAspectRatio={"xMinYMin"}>
-      <image width={size.width} height={size.height} href={svgPath} />
+      <image width={size.width} height={size.height} href={basemapImage} />
       <g>
         <circle
-          cx={marker.cx}
-          cy={marker.cy}
+          cx={projectedCoords[0]}
+          cy={projectedCoords[1]}
           r={marker.radius}
           class={styles.circle}
         />
--- src/lib/components/particles/map-locator/maplocator.stories.jsx
+++ src/lib/components/particles/map-locator/maplocator.stories.jsx
@@ -2,9 +2,8 @@ import { MapLocator } from '.'
 import ukmap from './ukmap.json'
 import { feature } from 'topojson-client'
 import proj4d3 from 'proj4d3'
-import { geoPath } from 'd3-geo'
 
- const meta = {
+const meta = {
   title: 'Particles/MapLocator',
   component: MapLocator,
 }
@@ -13,34 +12,22 @@ const featureCollection = {
   type: 'FeatureCollection',
   features: feature(ukmap, ukmap.objects.ukmap).features
 }
+
 const width = 200
 const height = 300
-const size = { width, height }
+
 const proj4String = '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs'
-const coordinates = [0.1276, 51.5072]
-const radius = 15
-
-const proj = proj4d3(proj4String).fitSize([width, height], featureCollection)
-const path = geoPath().projection(proj)
-
-const cx = proj(coordinates)[0]
-const cy = proj(coordinates)[1]
-
-const marker = { cx, cy, radius }
+const projection = proj4d3(proj4String).fitSize([width, height], featureCollection)
 
 export default meta
 
 export const Default = {
   args: {
-    projection: proj,
-    path,
-    size,
-    features: featureCollection.features,
-    marker,
-    svgPath: 'src/lib/components/particles/map-locator/uk_map.svg',
-    styles: {
-      circle: 'fill-color--con'
-    }
+    projection,
+    size: { width, height },
+    marker: { radius: 15, coordinates: [0.1276, 51.5072] },
+    basemapImage: 'src/lib/components/particles/map-locator/example.svg',
+    styles: { circle: 'fill-color--con' }
   },
   render: (args) => <MapLocator {...args} />
 }
