1df3184203cfa0c8c6a4c840ce9ac7d0d8a83282
Antonio Voce <antonio.voce@guardian.co.uk>
2024-03-07 15:17:16 +0000

    Locator (#15)
    * add dynamic locator
    
    * remove external dependencies
    
    * add style
    
    * style naming change
    
    * change size and marker props
    
    * agnostic circle
    
    * clean up
    
    * remove useless file and use generic naming for json

---
+++ src/lib/components/particles/map-locator/index.jsx
@@ -0,0 +1,24 @@
+import defaultStyles from "./style.module.css"
+import { mergeStyles } from "$styles/helpers/mergeStyles"
+
+export const MapLocator = ({ basemapImage, size, marker, styles, projection }) => {
+  styles = mergeStyles({ ...defaultStyles }, styles)
+  const projectedCoords = projection(marker.coordinates)
+  
+  return (
+    <svg
+      class={styles.svg}
+      viewBox={`0 0 ${size.width} ${size.height}`}
+      preserveAspectRatio={"xMinYMin"}>
+      <image width={size.width} height={size.height} href={basemapImage} />
+      <g>
+        <circle
+          cx={projectedCoords[0]}
+          cy={projectedCoords[1]}
+          r={marker.radius}
+          class={styles.circle}
+        />
+      </g>
+    </svg>
+  )
+}
