da8cbde45754d7b728557ec77af939cc9616201c
Seán Clarke <sean.clarke@guardian.co.uk>
2024-03-26 15:21:34 +0000

    New seat card and change bar components (#37)
    * slightly suspicious commit
    
    * fri pm working in storybook but not repo
    
    * change bar working
    
    * add noc colours
    
    * review based changes

---
+++ src/lib/components/particles/change-bar/change-bar.stories.jsx
@@ -0,0 +1,16 @@
+import {ChangeBar} from '.'
+
+export default {
+    title: 'Particles/ChangeBar',
+    component: ChangeBar
+}
+
+export const Default = {
+    args: {
+      fraction: 0.4,
+      party: "lab",
+      positive: true,
+      width: 200,
+      height: "1em"
+    }
+}
