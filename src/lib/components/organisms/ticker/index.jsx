b422fe27c671907d0f35d276ff919ddf8068ea0e
Niels de Hoog <njdehoog@gmail.com>
2024-03-28 16:12:52 +0000

    Fix next button state

--- src/lib/components/organisms/ticker/index.jsx
+++ src/lib/components/organisms/ticker/index.jsx
@@ -1,32 +1,48 @@
 import { toChildArray } from 'preact'
 import { useState, useRef, useLayoutEffect } from 'preact/hooks'
+import { useWindowSize } from '$shared/hooks/useWindowSize'
 import { Gradient } from './gradient'
 import { ArrowButton } from '$particles/arrow-button/index'
 import styles from './style.module.scss'
 
-function totalWidthForElements(elements) {
-  return Array.from(elements).reduce((totalWidth, element) => {
-    totalWidth += element.clientWidth
-    return totalWidth
-  }, 0)
+function totalSizeForElements(elements) {
+  return Array.from(elements).reduce(
+    (totalSize, element) => {
+      totalSize.width += element.clientWidth
+      totalSize.height += element.clientHeight
+      return totalSize
+    },
+    { width: 0, height: 0 },
+  )
 }
 
 export function Ticker({ children }) {
   const [offset, setOffset] = useState(0)
+  const windowSize = useWindowSize()
 
   const tickerItemsRef = useRef()
   const [hideButtons, setHideButtons] = useState(false)
+  const [nextButtonDisabled, setNextButtonDisabled] = useState(false)
 
   const childArray = toChildArray(children)
 
   useLayoutEffect(() => {
     const tickerItemsContainer = tickerItemsRef.current
     if (tickerItemsContainer) {
-      const totalWidth = totalWidthForElements(tickerItemsContainer.children)
-      const hideButtons = totalWidth < tickerItemsContainer.clientWidth
+      const { width, height } = totalSizeForElements(tickerItemsContainer.children)
+      const hideButtons =
+        windowSize.width >= 480
+          ? width <= tickerItemsContainer.clientWidth
+          : height <= tickerItemsContainer.clientHeight
       setHideButtons(hideButtons)
+
+      if (tickerItemsContainer.children.length > 0) {
+        const itemWidth = tickerItemsContainer.children[0].clientWidth
+        const nextButtonDisabled = offset * itemWidth > width - tickerItemsContainer.clientWidth
+        setNextButtonDisabled(nextButtonDisabled)
+      }
     }
-  }, [setHideButtons, childArray])
+  }, [offset, windowSize, childArray, setHideButtons, setNextButtonDisabled])
 
   return (
     <div className={styles.ticker} style={`--ticker-offset: ${offset}`}>
@@ -37,12 +53,12 @@ export function Ticker({ children }) {
           </div>
         ))}
       </div>
-      <div className={styles.controls}>
+      <div className={styles.controls} style={hideButtons && { display: 'none' }}>
         <div className={styles.gradient}>
           <Gradient />
         </div>
-        <div className={styles.buttons} style={hideButtons && { display: 'none' }}>
-          <ArrowButton onClick={() => setOffset((d) => d + 1)} disabled={offset >= childArray.length - 4} />
+        <div className={styles.buttons}>
+          <ArrowButton onClick={() => setOffset((d) => d + 1)} disabled={nextButtonDisabled} />
           <ArrowButton direction="left" onClick={() => setOffset((d) => d - 1)} disabled={offset <= 0} />
         </div>
       </div>
