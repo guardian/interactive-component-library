1eddbfa61b789267b64b3cca3962ab1872f369cd
Niels de Hoog <njdehoog@gmail.com>
2024-03-28 11:36:12 +0000

    Add gradient

--- src/lib/components/organisms/ticker/index.jsx
+++ src/lib/components/organisms/ticker/index.jsx
@@ -1,5 +1,20 @@
+import { toChildArray } from 'preact'
+import { Gradient } from './gradient'
 import styles from './style.module.css'
 
 export function Ticker({ children }) {
-  return <div class={styles.ticker}>{children}</div>
+  return (
+    <div className={styles.ticker}>
+      <div className={styles.tickerItems}>
+        {toChildArray(children).map((child, index) => (
+          <div className={styles.tickerItem} key={index}>
+            {child}
+          </div>
+        ))}
+      </div>
+      <div className={styles.gradient}>
+        <Gradient />
+      </div>
+    </div>
+  )
 }
