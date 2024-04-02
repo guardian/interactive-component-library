5a9d6bc6a6b5f514333cc0482c44b625788a6e34
Niels de Hoog <njdehoog@gmail.com>
2024-04-02 11:24:59 +0100

    Add mobile button to ticker

--- src/lib/components/particles/button/index.jsx
+++ src/lib/components/particles/button/index.jsx
@@ -1,8 +1,12 @@
 import defaultStyles from './style.module.css'
 import { mergeStyles } from '$styles/helpers/mergeStyles'
 
-export function Button({ styles, children }) {
+export function Button({ type = 'regular', styles, children }) {
   styles = mergeStyles(defaultStyles, styles)
 
-  return <button className={styles.button}>{children}</button>
+  return (
+    <button className={styles.button} data-type={type}>
+      <div className={styles.buttonInner}>{children}</div>
+    </button>
+  )
 }
