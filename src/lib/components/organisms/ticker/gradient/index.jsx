782649785b37d97bb1bead7a62e64d3f695a2cef
Niels de Hoog <njdehoog@gmail.com>
2024-04-02 11:44:14 +0100

    Set gradient colours in CSS

--- src/lib/components/organisms/ticker/gradient/index.jsx
+++ src/lib/components/organisms/ticker/gradient/index.jsx
@@ -15,8 +15,8 @@ export function Gradient() {
       </g>
       <defs>
         <linearGradient id="paint0_linear_3798_6653" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop stop-color="#F6F6F6" stop-opacity="0" />
-          <stop offset="1" stop-color="#F6F6F6" />
+          <stop className={styles.firstStop} />
+          <stop className={styles.lastStop} offset="1" />
         </linearGradient>
       </defs>
     </svg>
