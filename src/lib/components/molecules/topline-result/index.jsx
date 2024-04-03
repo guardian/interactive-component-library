7a9b92c555929f6406474def17cf97cc986bc404
Niels de Hoog <njdehoog@gmail.com>
2024-04-03 08:50:31 +0100

    Finish refactor

--- src/lib/components/molecules/topline-result/index.jsx
+++ src/lib/components/molecules/topline-result/index.jsx
@@ -1,13 +1,28 @@
-import defaultStyles from "./style.module.scss"
-import { mergeStyles } from "$styles/helpers/mergeStyles"
+import { InfoButton } from '$particles'
+import { mergeStyles } from '$styles/helpers/mergeStyles'
+import defaultStyles from './style.module.scss'
 
-export const ToplineResult = ({ name, mainNumber, secondaryNumber, children, styles, displayRow=false, abbreviation, onMouseOver }) => {
+export const ToplineResult = ({
+  name,
+  mainNumber,
+  secondaryNumber,
+  styles,
+  displayRow = false,
+  abbreviation,
+  onMouseOver,
+  onInfoPress,
+}) => {
   styles = mergeStyles({ ...defaultStyles }, styles)
   const displayStyle = displayRow ? styles.displayRow : styles.displayColumn
 
   return (
     <div class={styles.toplineResult} onMouseOver={onMouseOver}>
-      <div class={styles.topRow}><span class={`${styles.name} before-color--${abbreviation}`}>{name}</span>{children}</div>
+      <div class={styles.topRow}>
+        <span class={`${styles.name} before-color--${abbreviation}`}>{name}</span>{' '}
+        <span class={styles.infoButton}>
+          <InfoButton onClick={onInfoPress} />
+        </span>
+      </div>
       <div class={`${styles.displayNumbers} ${displayStyle}`}>
         <div class={styles.mainNumber}>{mainNumber}</div>
         <div class={styles.secondaryNumber}>{secondaryNumber}</div>
