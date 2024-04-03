a4c753a6ca58febf42c315a10aba176dbcce6394
Niels de Hoog <njdehoog@gmail.com>
2024-04-03 15:57:08 +0100

    Add suggestions

--- src/lib/components/molecules/search/search.stories.jsx
+++ src/lib/components/molecules/search/search.stories.jsx
@@ -1,8 +1,13 @@
+import { action } from '@storybook/addon-actions'
 import { Search } from '.'
 
 const meta = {
   title: 'Molecules/Search',
   component: Search,
+  args: {
+    onSelect: action('select'),
+    onSubmit: action('submit'),
+  },
 }
 
 export default meta
@@ -10,5 +15,17 @@ export default meta
 export const Default = {
   args: {
     placeholder: 'Search',
+  },
+}
+
+export const WithSuggestions = {
+  args: {
+    placeholder: 'Search',
+    inputValue: 'Sugg',
+    suggestions: [
+      { text: 'Suggestion 1', value: 1 },
+      { text: 'Suggestion 2', value: 2 },
+      { text: 'Suggestion 3', value: 3 },
+    ],
   },
 }
