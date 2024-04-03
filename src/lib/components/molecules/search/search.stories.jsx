7dd68ff0a6b43214bb005fedcf10b413b4fb1f9b
Niels de Hoog <njdehoog@gmail.com>
2024-04-03 14:52:08 +0100

    Create Search component

---
+++ src/lib/components/molecules/search/search.stories.jsx
@@ -0,0 +1,14 @@
+import { Search } from '.'
+
+const meta = {
+  title: 'Molecules/Search',
+  component: Search,
+}
+
+export default meta
+
+export const Default = {
+  args: {
+    placeholder: 'Search',
+  },
+}
