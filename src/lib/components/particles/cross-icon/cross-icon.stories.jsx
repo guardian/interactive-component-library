a10212cee6a9b896c147f4be1f5d9b6197e28e12
Niels de Hoog <njdehoog@gmail.com>
2024-04-05 09:02:23 +0100

    Create cross icon

---
+++ src/lib/components/particles/cross-icon/cross-icon.stories.jsx
@@ -0,0 +1,17 @@
+import { CrossIcon } from '.'
+
+const meta = {
+  title: 'Particles/CrossIcon',
+  component: CrossIcon,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 17, height: 17 }}>
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default meta
+
+export const Default = {}
