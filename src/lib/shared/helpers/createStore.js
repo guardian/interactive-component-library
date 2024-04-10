87bd11bcd2151520725e3063ef135a71b86d809a
Niels de Hoog <njdehoog@gmail.com>
2024-04-10 11:58:48 +0100

    Render GradientIcon defs only once

---
+++ src/lib/shared/helpers/createStore.js
@@ -0,0 +1,46 @@
+import { useCallback } from 'preact/hooks'
+import { useSyncExternalStore } from 'preact/compat'
+import shouldUpdate from './shouldUpdate'
+
+export default function createStore(initialStore) {
+  let store = initialStore
+  const listeners = new Set()
+
+  function useStore(selectorFn = (store) => store) {
+    const subscribe = useCallback((updater) => {
+      const listener = {
+        updater,
+        selectorFn,
+      }
+      listeners.add(listener)
+      return () => {
+        listeners.delete(listener)
+      }
+    }, [])
+
+    const syncedStore = useSyncExternalStore(subscribe, getStore, getServerStore)
+    return selectorFn(syncedStore)
+  }
+
+  function setStore(action) {
+    const oldStore = store
+
+    store = action instanceof Function ? action(store) : action
+
+    listeners.forEach(({ selectorFn, updater }) => {
+      const oldState = selectorFn(oldStore)
+      const newState = selectorFn(store)
+      if (shouldUpdate(oldState, newState)) updater(() => newState)
+    })
+  }
+
+  function getStore() {
+    return store
+  }
+
+  function getServerStore() {
+    return initialStore
+  }
+
+  return [useStore, setStore, getStore]
+}
