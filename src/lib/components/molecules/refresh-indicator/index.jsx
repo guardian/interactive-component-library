1344b706df390923e112dd3edf076c34d7c701d8
Niels de Hoog <njdehoog@gmail.com>
2024-04-12 08:45:46 +0100

    Change refresh indicator props

--- src/lib/components/molecules/refresh-indicator/index.jsx
+++ src/lib/components/molecules/refresh-indicator/index.jsx
@@ -2,13 +2,13 @@ import { CircleIcon } from '$particles'
 import { mergeStyles } from '$styles/helpers/mergeStyles'
 import defaultStyles from './style.module.css'
 
-export function RefreshIndicator({ secondsUntilNext, styles }) {
+export function RefreshIndicator({ text, styles }) {
   styles = mergeStyles(defaultStyles, styles)
   return (
     <div className={styles.refreshIndicator}>
       <CircleIcon pulse={true} />
       <span className={styles.liveText}>LIVE</span>
-      <span className={styles.refreshText}>next refresh in {secondsUntilNext}s</span>
+      <span className={styles.refreshText}>{text}</span>
     </div>
   )
 }
