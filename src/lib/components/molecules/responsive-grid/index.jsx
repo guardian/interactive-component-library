2eb51662620f856aba27f6e5d81339991874a9f7
Niels de Hoog <njdehoog@gmail.com>
2024-04-15 10:36:18 +0100

    Create ResponsiveGrid component (#73)
    * Finish creating responsive grid
    
    * Add custom style prop
    
    ---------
    
    Co-authored-by: Antonio Voce <antonio.voce@guardian.co.uk>

---
+++ src/lib/components/molecules/responsive-grid/index.jsx
@@ -0,0 +1,25 @@
+import { toChildArray } from 'preact'
+import defaultStyles from './style.module.scss'
+import { mergeStyles } from '$styles/helpers/mergeStyles'
+
+export const GridType = {
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+}
+
+export function ResponsiveGrid({ type = GridType.medium, children, styles }) {
+  styles = mergeStyles({ ...defaultStyles }, styles)
+
+  return (
+    <div className={styles.grid} data-grid-type={type}>
+      {toChildArray(children).map((child, index) => {
+        return (
+          <div key={index} className={styles.gridItem}>
+            {child}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
