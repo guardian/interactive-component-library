a05bf96c818f58bb39fa474e8b38700a1c5f5892
Niels de Hoog <njdehoog@gmail.com>
2024-04-16 07:59:49 +0100

    Find features by layer

--- src/lib/components/molecules/map/context/SVGMapProvider.jsx
+++ src/lib/components/molecules/map/context/SVGMapProvider.jsx
@@ -1,7 +1,8 @@
-import { useMemo } from 'preact/hooks'
+import { useMemo, useState, useCallback } from 'preact/hooks'
 import { geoPath } from 'd3-geo'
 import { bboxFeature } from '../helpers/bboxFeature'
 import { MapContext } from './MapContext'
+import { calculateScale } from '../helpers/geoMath'
 
 export function SVGMapProvider({ id, mapRef, width, height, padding, config, zoom, selectedFeature, children }) {
   const contentSize = useMemo(
@@ -18,9 +19,34 @@ export function SVGMapProvider({ id, mapRef, width, height, padding, config, zoo
 
   const path = geoPath().projection(projection)
 
+  const [layerSet, setLayers] = useState(new Set())
+
+  const findFeatureAtPoint = useCallback(
+    ({ x, y }) => {
+      const adjustedPoint = [x - padding.left, y - padding.top]
+      for (const layer of layerSet) {
+        if (typeof layer.findFeatureAtPoint === 'function') {
+          const feature = layer.findFeatureAtPoint(adjustedPoint)
+          if (feature) return feature
+        }
+      }
+      return null
+    },
+    [padding, layerSet],
+  )
+
+  const getVisibleBounds = useCallback(() => {
+    const projectedSW = projection.invert([padding.left, contentSize.height])
+    const projectedNE = projection.invert([padding.left + contentSize.width, padding.top])
+    return [projectedSW, projectedNE]
+  }, [projection, contentSize, padding])
+
+  const getZoomScale = useCallback(() => {
+    return calculateScale(getVisibleBounds(), contentSize.width, contentSize.height)
+  }, [getVisibleBounds, contentSize])
+
   const context = {
     id,
-    mapRef,
     projection,
     config,
     path,
@@ -33,6 +59,23 @@ export function SVGMapProvider({ id, mapRef, width, height, padding, config, zoo
       [width, height],
     ],
     selectedFeature,
+    getZoomScale,
+    registerLayer(layer) {
+      setLayers((current) => {
+        current.add(layer)
+        return current
+      })
+    },
+    unregisterLayer(layer) {
+      setLayers((current) => {
+        current.delete(layer)
+        return current
+      })
+    },
+    findFeatureAtPoint,
   }
+
+  mapRef.current = context
+
   return <MapContext.Provider value={context}>{children}</MapContext.Provider>
 }
