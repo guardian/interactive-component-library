3fb3492aabfd6961a2029665fd7c7506a6fed0af
Niels de Hoog <njdehoog@gmail.com>
2024-04-19 15:25:49 +0100

    App fixes

--- src/lib/components/particles/stacked-grid/index.jsx
+++ src/lib/components/particles/stacked-grid/index.jsx
@@ -1,19 +1,15 @@
-import defaultStyles from "./style.module.css"
-import { mergeStyles } from "$styles/helpers/mergeStyles"
-
+import defaultStyles from './style.module.css'
+import { mergeStyles } from '$styles/helpers/mergeStyles'
 
 const getItemRotate = (fromBottom, fromLeft) => {
-  if(fromBottom && fromLeft) {
+  if (fromBottom && fromLeft) {
     return 'stackedGridRotateItems270'
-  } 
-  else if(fromBottom && !fromLeft) {
+  } else if (fromBottom && !fromLeft) {
     return 'stackedGridRotateItems180'
-  }
-  else if(!fromBottom && !fromLeft) {
-    return 'stackedGridRotateItems90' 
-  }
-  else if(!fromBottom && fromLeft){
-    return 'stackedGridRotateItems0'  // the css default
+  } else if (!fromBottom && !fromLeft) {
+    return 'stackedGridRotateItems90'
+  } else if (!fromBottom && fromLeft) {
+    return 'stackedGridRotateItems0' // the css default
   }
 }
 
@@ -21,34 +17,21 @@ const getItemRotate = (fromBottom, fromLeft) => {
 // this works better than changing flex direction because it allows for an X and Y change
 
 const getContainerFlip = (fromBottom, fromLeft) => {
-  let scaleX = fromLeft ? 1 : -1 ;
-  let scaleY = fromBottom ? -1 : 1 ;
+  let scaleX = fromLeft ? 1 : -1
+  let scaleY = fromBottom ? -1 : 1
   return `scale(${scaleX}, ${scaleY})`
 }
 
-
-export const StackedGrid = ({ fromLeft, fromBottom, containerWidth, children, styles }) => {
-
-
+export const StackedGrid = ({ fromLeft, fromBottom, children, styles }) => {
   // CSS  attributes driven by args
-  let containerFlip = getContainerFlip(fromBottom, fromLeft);
-  let itemRotateClass = getItemRotate(fromBottom, fromLeft);
-
-
-  let flexStyles = {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    width: containerWidth, 
-    transform: containerFlip,
-  }
+  let containerFlip = getContainerFlip(fromBottom, fromLeft)
+  let itemRotateClass = getItemRotate(fromBottom, fromLeft)
 
   styles = mergeStyles(defaultStyles, styles)
 
-
   return (
-      <div style={flexStyles} className={`${styles.stackedGridContainer} ${styles[itemRotateClass]}`}>
-        {children}
-      </div>
+    <div style={{ transform: containerFlip }} className={`${styles.stackedGridContainer} ${styles[itemRotateClass]}`}>
+      {children}
+    </div>
   )
 }
