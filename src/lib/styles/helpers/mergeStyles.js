4d90dff56bbfa85174c0d2805691265fa7ffcdf8
Niels de Hoog <njdehoog@gmail.com>
2024-05-07 11:52:39 +0100

    Write tests for style merge function and fix nested merge

--- src/lib/styles/helpers/mergeStyles.js
+++ src/lib/styles/helpers/mergeStyles.js
@@ -1,4 +1,5 @@
 export function mergeStyles(firstStyle, secondStyle) {
+  if (!firstStyle) return secondStyle
   if (!secondStyle) return firstStyle
   if (typeof firstStyle === 'string') {
     if (typeof secondStyle !== 'string') {
@@ -9,16 +10,7 @@ export function mergeStyles(firstStyle, secondStyle) {
 
   const allKeys = new Set([...Object.keys(firstStyle), ...Object.keys(secondStyle)])
   const merged = Array.from(allKeys).reduce((result, key) => {
-    const classNames = []
-    if (firstStyle && key in firstStyle) {
-      classNames.push(firstStyle[key])
-    }
-
-    if (secondStyle && key in secondStyle) {
-      classNames.push(secondStyle[key])
-    }
-
-    result[key] = classNames.join(' ')
+    result[key] = mergeStyles(firstStyle[key], secondStyle[key])
     return result
   }, {})
 
