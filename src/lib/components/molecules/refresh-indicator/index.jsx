1d38eb28e7f433a4ff8908d63ee5ada65dbf3a5f
Niels de Hoog <njdehoog@gmail.com>
2024-05-08 13:14:07 +0100

    Fix refresh indicator

--- src/lib/components/molecules/refresh-indicator/index.jsx
+++ src/lib/components/molecules/refresh-indicator/index.jsx
@@ -1,12 +1,14 @@
 import { CircleIcon } from '$particles'
 import { mergeStyles } from '$styles/helpers/mergeStyles'
-import defaultStyles from './style.module.css'
+import defaultStyles from './style.module.scss'
 
 export function RefreshIndicator({ text, styles }) {
   styles = mergeStyles(defaultStyles, styles)
   return (
     <div className={styles.refreshIndicator}>
-      <CircleIcon pulse={true} />
+      <span className={styles.icon}>
+        <CircleIcon pulse={true} />
+      </span>
       <span className={styles.liveText}>LIVE</span>
       <span className={styles.refreshText}>{text}</span>
     </div>
