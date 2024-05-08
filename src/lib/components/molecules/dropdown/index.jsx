698270c7f12022f85893de5faee84578d4eb4bdc
Niels de Hoog <njdehoog@gmail.com>
2024-05-08 09:24:51 +0100

    Complete basic dropdown

--- src/lib/components/molecules/dropdown/index.jsx
+++ src/lib/components/molecules/dropdown/index.jsx
@@ -1,14 +1,59 @@
-import { useState } from "preact/hooks"
-import { Chevron } from "$particles"
-import styles from "./style.module.css"
+import { useState, useCallback, useMemo } from 'preact/hooks'
+import { Chevron } from '$particles'
+import styles from './style.module.css'
 
-export function Dropdown({ icon, title}) {
-  const [expanded, setExpanded] = useState(false)
+export function Dropdown({ title, hint, options, onSelect }) {
+  const [expanded, setExpanded] = useState(true)
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const onOptionClick = useCallback((option, index) => {
+    setSelectedIndex(index)
+    if (onSelect) onSelect(option, index)
+  }, [onSelect])
+
+  const iconForSelectedOption = useMemo(() => {
+    const selectedOption = options[selectedIndex]
+    return selectedOption.icon
+  }, [options, selectedIndex])
+  
   return (
-    <button className={styles.button} onClick={() => setExpanded(current => !current)}>
-      <img src={icon} className={styles.icon} />
-      <span className={styles.title}>{title}</span>
-      <Chevron active={true} size="large" direction={expanded ? 'up' : 'down'} />
-    </button>
+    <div>
+      <button className={styles.button} onClick={() => setExpanded((current) => !current)}>
+        <img src={iconForSelectedOption} className={styles.icon} />
+        <span className={styles.title}>{title}</span>
+        <Chevron active={true} size="large" direction={expanded ? 'up' : 'down'} />
+      </button>
+
+      <div className={styles.popout} style={{ visibility: expanded ? "visible" : "hidden" }}>
+        {hint && <p className={styles.hint}>{hint}</p>}
+        {options.map((option, index) => (
+          <button key={option.title} className={styles.option} onClick={() => onOptionClick(option, index)}>
+            <img src={option.icon} className={styles.optionIcon} />
+            <div className={styles.optionText}>
+              <h4 className={styles.optionTitle}>{option.title}</h4>
+              <p className={styles.optionDescription}>{option.description}</p>
+            </div>
+            {index === selectedIndex && (
+              <div className={styles.checkmark}>
+                <Checkmark />
+              </div>
+            )}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function Checkmark() {
+  return (
+    <svg width="11" height="9" viewBox="0 0 11 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0.631814 4.43687L0.0839844 4.9847L2.82313 8.8195H3.08335L10.9173 0.711624L10.3695 0.17749L3.08335 6.77884L0.631814 4.43687Z"
+        className={styles.checkmarkPath}
+      />
+    </svg>
   )
 }
