160192adf9c29ea0a895ae4a3714f65839080990
Niels de Hoog <njdehoog@gmail.com>
2024-05-09 08:56:07 +0100

    Simplify tooltip

--- src/lib/components/molecules/tooltip/tooltip.stories.jsx
+++ src/lib/components/molecules/tooltip/tooltip.stories.jsx
@@ -1,4 +1,4 @@
-import { Tooltip, TooltipType } from '.'
+import { Tooltip } from '.'
 import { InfoButton } from '$particles'
 import { useEffect, useRef, useState } from 'preact/hooks'
 
@@ -11,19 +11,11 @@ export const Default = {
   render: () => <TooltipPreview />,
 }
 
-export const Modal = {
-  render: () => <TooltipPreview type={TooltipType.modal} />,
-}
-
-export const Overflow = {
-  render: () => <TooltipOverflow />,
-}
-
 export const ButtonPress = {
   render: () => <TooltipForButton />,
 }
 
-function TooltipPreview({ type }) {
+function TooltipPreview() {
   const tooltipped = useRef(null)
   const [element, setElement] = useState()
 
@@ -35,7 +27,7 @@ function TooltipPreview({ type }) {
     <>
       <div ref={tooltipped} style="width: 100%; height: 300px; background-color: #CCC" />
       {element && (
-        <Tooltip for={element} renderIn="#storybook-root" type={type}>
+        <Tooltip for={element}>
           {({ x, y }) => (
             <div style="border-top: 1px solid #333; background-color: #FFF; padding: 10px;">
               <p>Tooltip position</p>
@@ -50,55 +42,19 @@ function TooltipPreview({ type }) {
   )
 }
 
-function TooltipOverflow() {
-  const tooltipped = useRef(null)
-  const [element, setElement] = useState()
-
-  useEffect(() => {
-    setElement(tooltipped.current)
-  }, [tooltipped])
-
-  return (
-    <>
-      <div style="width: 100%; padding: 20px; background-color: #EAF2F8; font-family: var(--text-sans); border: 1px solid #CCC;">
-        <p style={{ marginBottom: 10 }}>Tooltip is rendered in this element</p>
-        <div ref={tooltipped} style="width: 100%; height: 300px; padding: 20px; background-color: #2980B9;">
-          <p>
-            Tooltip will show outside of tooltipped element, but never outside the bounds of the element it is rendered
-            in.
-          </p>
-        </div>
-      </div>
-
-      {element && (
-        <Tooltip for={element} renderIn="#storybook-root">
-          {({ x, y }) => (
-            <div style="border: 1px solid #333; background-color: #FFF; padding: 10px;">
-              <p>Tooltip position</p>
-              <p>
-                x: {x}, y: {y}
-              </p>
-            </div>
-          )}
-        </Tooltip>
-      )}
-    </>
-  )
-}
-
-function TooltipForButton() {
+function TooltipForButton({ event = "click" }) {
   const [tooltipTarget, setTooltipTarget] = useState()
 
   useEffect(() => {
     function onClick() {
       setTooltipTarget(null)
     }
-    window.addEventListener('click', onClick)
+    window.addEventListener(event, onClick)
 
     return () => {
-      window.removeEventListener('click', onClick)
+      window.removeEventListener(event, onClick)
     }
-  }, [])
+  }, [event])
 
   return (
     <div style={{ height: '100vh' }}>
@@ -112,7 +68,7 @@ function TooltipForButton() {
         }}
       />
       {tooltipTarget && (
-        <Tooltip for={tooltipTarget} renderIn="#storybook-root">
+        <Tooltip for={tooltipTarget}>
           <div style="border: 1px solid #333; background-color: #FFF; padding: 10px;">Tooltip</div>
         </Tooltip>
       )}
