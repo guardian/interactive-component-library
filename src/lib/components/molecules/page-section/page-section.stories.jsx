32fcf3e2e2c9d7d4e058a1c9437a49ecbcb22517
Niels de Hoog <njdehoog@gmail.com>
2024-05-15 14:40:00 +0100

    Set min height based on header content

--- src/lib/components/molecules/page-section/page-section.stories.jsx
+++ src/lib/components/molecules/page-section/page-section.stories.jsx
@@ -66,3 +66,37 @@ export const BackgroundColor = {
     borderTop: true,
   },
 }
+
+export const SectionWithLongHeaderContent = {
+  render: (args) => {
+    return (
+      <>
+        <PageSection {...args}>
+          {{
+            header: (
+              <div>
+                <h2>Section one</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla, libero vel fermentum
+                  dapibus, neque eros interdum magna, at cursus enim justo nec elit. Orci varius natoque penatibus et
+                  magnis dis parturient montes, nascetur ridiculus mus. Maecenas accumsan sem lacinia turpis
+                  ullamcorper, imperdiet dictum justo euismod. Quisque justo sem, porta non dictum eget, pharetra a
+                  erat. Quisque sollicitudin aliquet sem, sed feugiat tortor luctus id. Maecenas tincidunt sapien sit
+                  amet urna varius finibus. Nunc finibus arcu ac elementum gravida. Aenean blandit ut arcu eget ornare.
+                  Curabitur in malesuada ipsum, ut aliquet est.
+                </p>
+              </div>
+            ),
+            content: <p className={styles.content}>Section content</p>,
+          }}
+        </PageSection>
+        <PageSection borderTop={true}>
+          {{
+            header: <h2>Section two</h2>,
+            content: <p className={styles.content}>Section content</p>,
+          }}
+        </PageSection>
+      </>
+    )
+  },
+}
