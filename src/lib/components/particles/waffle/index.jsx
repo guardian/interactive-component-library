bff241c82ad710ac0f88134b7a7cea473ee68f17
Antonio Voce <antonio.voce@guardian.co.uk>
2024-05-16 14:01:43 +0100

    more changes

--- src/lib/components/particles/waffle/index.jsx
+++ src/lib/components/particles/waffle/index.jsx
@@ -1,5 +1,7 @@
 import defaultStyles from "./style.module.css"
 import { mergeStyles } from "$styles/helpers/mergeStyles"
+import { useContainerSize } from '$shared/hooks/useContainerSize'
+import { useRef } from 'preact/hooks'
 
 export const WaffleType = {
   circle: 'circle',
@@ -9,8 +11,10 @@ export const WaffleType = {
 const WaffleUnit = ({ type, attributes }) => type === WaffleType.square ? <rect {...attributes} /> : <circle {...attributes} />
 
 export const Waffle = ({ units, numberOfRows, type = WaffleType.circle, idAccessor = 'party', onMouseOver, paddingTop, showHalfLine, styles }) => {
-  //units is an array of objects with the following structure: { [idAccessor]: 'A' }.
-  const width = 1300
+
+  const containerRef = useRef()
+  const containerSize = useContainerSize(containerRef)
+  const width = containerSize ? containerSize.width : 0
   const total = units.length
   const columns = Math.ceil(total / numberOfRows)
   const unitWidth = width / columns
@@ -19,24 +23,47 @@ export const Waffle = ({ units, numberOfRows, type = WaffleType.circle, idAccess
   
   styles = mergeStyles(defaultStyles, styles)
 
+  
   return (
-    <svg viewBox={`0 0 ${width} ${height}`} class={styles.svg}>
-      <g>
-        {
-          units.map((unit, j) => {
-            const attributes = type === WaffleType.square ?
-            { onMouseOver: e => onMouseOver(unit, e), class: `${styles.unit} fill-color--${unit[idAccessor]}`, height: unitHeight, width: unitWidth, x: unitWidth * Math.floor(j / numberOfRows), y: unitHeight * (j % numberOfRows) + paddingTop} :
-            { onMouseOver: e => onMouseOver(unit, e), class: `${styles.unit} fill-color--${unit[idAccessor]}`, r: unitWidth / 2, transform: `translate(${unitWidth * Math.floor(j / numberOfRows) + unitWidth / 2}, ${unitHeight * (j % numberOfRows) + unitHeight / 2 + paddingTop})` }
+    <div ref={containerRef} className={styles.container}>
+      { containerSize &&
+        <svg viewBox={`0 0 ${width} ${height}`} class={styles.svg}>
+          <g>
+            {
+              units.map((unit, j) => {
+                let attributes
+                
+                if (type === WaffleType.square) {
+                  attributes = {
+                    id: unit[idAccessor] || `w-${j}`,
+                    onMouseOver: e => onMouseOver(unit, e),
+                    class: `${styles.unit} ${unit.class}`,
+                    height: unitHeight,
+                    width: unitWidth,
+                    x: unitWidth * Math.floor(j / numberOfRows),
+                    y: unitHeight * (j % numberOfRows) + paddingTop
+                  }
+                } else {
+                  attributes = {
+                    id: unit[idAccessor] || `w-${j}`,
+                    onMouseOver: e => onMouseOver(unit, e),
+                    class: `${styles.unit} ${unit.class}`,
+                    r: unitWidth / 2,
+                    transform: `translate(${unitWidth * Math.floor(j / numberOfRows) + unitWidth / 2}, ${unitHeight * (j % numberOfRows) + unitHeight / 2 + paddingTop})`
+                  }
+                }
 
-            return <WaffleUnit key={`wu-${j}`} type={type} attributes={attributes} />
-          })
-        }
-      </g>
-      {showHalfLine &&
-        <g>
-          <line class={styles.line} x1={width / 2} x2={width / 2} y1={0} y2={height} stroke='#121212' />
-        </g>
+                return <WaffleUnit key={`wu-${j}`} type={type} attributes={attributes} />
+              })
+            }
+          </g>
+          {showHalfLine &&
+            <g>
+              <line class={styles.line} x1={width / 2} x2={width / 2} y1={0} y2={height} stroke='#121212' />
+            </g>
+          }
+        </svg>
       }
-    </svg>
+    </div>
   )
 }
--- src/lib/components/particles/waffle/waffle.stories.jsx
+++ src/lib/components/particles/waffle/waffle.stories.jsx
@@ -1,11 +1,5 @@
 import { Waffle, WaffleType } from '.'
 
-const meta = {
-  title: 'Particles/Waffle',
-  component: Waffle,
-}
-export default meta
-
 // this should also be the sort order, from bigger party to smaller with undeclared at the end
 const summary = {
   lab: 350,
@@ -17,25 +11,36 @@ const summary = {
 }
 
 // the output of the following line is: [{party: 'lab'}, {party: 'lab'}, ... {party: 'con'} ...]
-const partySeats = Object.keys(summary).map(party => Array(summary[party]).fill({ party })).flat()
+const partySeats = Object.keys(summary)
+  .map(party => Array(summary[party]).fill({ party, class: `fill-color--${party}` }))
+  .flat()
 
-const args = {
-  units: partySeats,
-  numberOfRows: 5,
-  idAccessor:'party',
-  paddingTop: 16,
-  onMouseOver: (a, b) => console.log(a, b),
-  showHalfLine: true
+const meta = {
+  title: 'Particles/Waffle',
+  component: Waffle,
+  args: {
+    units: partySeats,
+    numberOfRows: 5,
+    idAccessor: 'party',
+    paddingTop: 16,
+    onMouseOver: (a, b) => console.log(a, b),
+    showHalfLine: true,
+    type: WaffleType.circle
+  }
 }
+export default meta
 
-
-export const Circles = {
-  args,
-  render: (args) => <Waffle {...args} type={WaffleType.circle} />,
-}
 
 export const Squares = {
-  args,
-  render: (args) => <Waffle {...args} type={WaffleType.square} />,
+  component: Waffle,
+  args: {
+    type: WaffleType.square,
+  }
 }
 
+export const Circles = {
+  component: Waffle,
+  args: {
+    type: WaffleType.circle,
+  }
+}
