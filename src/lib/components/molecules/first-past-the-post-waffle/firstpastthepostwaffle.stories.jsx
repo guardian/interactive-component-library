b89856c3993a87481c17d78f94391b18361bb146
blosky <blosky@gmail.com>
2024-05-20 09:20:45 +0100

    scroll event to modal

---
+++ src/lib/components/molecules/first-past-the-post-waffle/firstpastthepostwaffle.stories.jsx
@@ -0,0 +1,62 @@
+import { FirstPastThePostWaffle } from '.'
+import { Waffle, WaffleType } from '$particles'
+
+export default {
+  title: 'Molecules/FPTPWaffle',
+  component: FirstPastThePostWaffle,
+}
+
+const summary = {
+  lab: 350,
+  con: 200,
+  snp: 50,
+  ld: 30,
+  green: 1,
+  undeclared: 19
+}
+
+// the output of the following line is: [{party: 'lab', class: fill-color-lab}, ...]
+const partySeats = Object.keys(summary)
+  .map(party => Array(summary[party]).fill({ party, class: `fill-color--${party}` }))
+  .flat()
+
+const waffleArgs = {
+  units: partySeats,
+  numberOfRows: 5,
+  idAccessor: 'party',
+  onMouseOver: (a, b) => console.log(a, b),
+  type: WaffleType.circle
+}
+
+const args = {
+  lineOverHang: 15
+}
+
+const longerLineArgs = {
+  lineOverHang: 30
+}
+
+export const Default = {
+  args,
+  render: (args) => (
+    <FirstPastThePostWaffle {...args}>
+      {{
+        waffle: <Waffle {...waffleArgs} />,
+        halfLineText: <p>326 to win</p>,
+      }}
+    </FirstPastThePostWaffle>
+  )
+}
+
+
+export const LongerLine = {
+  args: longerLineArgs,
+  render: (args) => (
+    <FirstPastThePostWaffle {...args}>
+      {{
+        waffle: <Waffle {...waffleArgs} />,
+        halfLineText: <p>326 to win</p>,
+      }}
+    </FirstPastThePostWaffle>
+  )
+}
--- src/lib/components/molecules/page-section/index.jsx
+++ src/lib/components/molecules/page-section/index.jsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'preact/compat'
+import { useLayoutEffect, useRef, useState } from 'preact/hooks'
 import { mergeStyles } from '$styles/helpers/mergeStyles'
 import defaultStyles from './style.module.scss'
 
@@ -6,13 +7,32 @@ export const PageSection = forwardRef(
   ({ children, styles, borderTop = false, backgroundColor = 'transparent' }, ref) => {
     styles = mergeStyles({ ...defaultStyles }, styles)
 
+    const [minHeight, setMinHeight] = useState('auto')
+    const headerRef = useRef()
+
+    useLayoutEffect(() => {
+      const headerElement = headerRef.current
+      setMinHeight(headerElement.offsetHeight)
+    }, [children])
+
+    useEffect(() => {
+      if (visible) {
+        window.addEventListener("scroll", onClick, { once: true });
+      }
+      return () => {
+        if (visible) {
+          window.removeEventListener("scroll", onClick);
+        }
+      };
+    }, [onClick, visible]);
+
     return (
       <section
         ref={ref}
         className={[styles.section, borderTop && styles.borderTop].join(' ')}
-        style={{ '--background-color': backgroundColor }}
+        style={{ '--background-color': backgroundColor, minHeight }}
       >
-        <div className={styles.header}>{children.header}</div>
+        <div className={styles.header} ref={headerRef}>{children.header}</div>
         <div className={styles.content}>{children.content}</div>
       </section>
     )
