624a7e32b59330e2fe9d6cc5192262b84565dfdc
Anna <ajwl@users.noreply.github.com>
2024-06-05 12:24:45 +0100

    add alt text to profile pic (#134)

--- src/lib/components/molecules/party-profile/index.jsx
+++ src/lib/components/molecules/party-profile/index.jsx
@@ -1,19 +1,19 @@
-import defaultStyles from './style.module.css'
-import { mergeStyles } from '$styles/helpers/mergeStyles'
+import defaultStyles from "./style.module.css"
+import { mergeStyles } from "$styles/helpers/mergeStyles"
 
 const SubtitleStyle = {
   small: "small",
   large: "large",
 }
 
-export const PartyProfile = ({ title, subtitle, subtitleStyle = SubtitleStyle.large, blurb, footnote, imgSrc, styles }) => {
+export const PartyProfile = ({ title, subtitle, subtitleStyle = SubtitleStyle.large, blurb, footnote, imgSrc, imgAltText, styles }) => {
   styles = mergeStyles({ ...defaultStyles }, styles)
 
   return (
     <div class={styles.container}>
-      <img src={imgSrc} className={styles.img} />
+      <img src={imgSrc} className={styles.img} alt={imgAltText} />
       <h3 className={styles.title}>{title}</h3>
-      <div className={[styles.subtitle, styles[subtitleStyle]].join(' ')}>{subtitle}</div>
+      <div className={[styles.subtitle, styles[subtitleStyle]].join(" ")}>{subtitle}</div>
       <div className={styles.blurb}>{blurb}</div>
       <div className={styles.footnote}>{footnote}</div>
     </div>
