3beb89131dbde7c987801116a525c69a6ec133f4
Antonio Voce <antonio.voce@guardian.co.uk>
2024-06-17 17:29:11 +0100

    revert to only containersize

--- src/lib/components/molecules/slope-chart/slopechart.stories.jsx
+++ src/lib/components/molecules/slope-chart/slopechart.stories.jsx
@@ -1,5 +1,4 @@
 import { SlopeChart } from "."
-import { ResponsiveGrid } from "../responsive-grid"
 
 const meta = {
   title: "Molecules/SlopeChart",
@@ -32,36 +31,4 @@ export const Default = {
     padding: { left: 24, right: 36, top: 20, bottom: 20 },
   },
   render: (args) => <SlopeChart {...args} />,
-}
-
-export const InGrid = {
-  args: {
-    id: "slope-chart",
-    domain: [0, 100],
-    lines: [
-      {
-        y1: 30,
-        y2: 10,
-        abbreviation: "snp",
-      },
-      {
-        y1: 20,
-        y2: 70,
-        abbreviation: "libdem",
-      },
-    ],
-    y2Label: (d) => `${d.y2}%`,
-    axis: {
-      startLabel: "2019",
-      endLabel: "2024",
-    },
-    padding: { left: 24, right: 36, top: 20, bottom: 20 },
-  },
-  render: (args) => (
-    <ResponsiveGrid type="large">
-      {new Array(5).fill(null).map((_) => (
-        <SlopeChart {...args} />
-      ))}
-    </ResponsiveGrid>
-  ),
 }
