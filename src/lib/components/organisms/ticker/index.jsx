71b0c78cd93a7e994b97e820c76fec2de8c5d53d
Anna Leach <anna.leach@guardian.co.uk>
2024-06-28 17:00:34 +0100

    show fewer button scrolls you up

--- src/lib/components/organisms/ticker/index.jsx
+++ src/lib/components/organisms/ticker/index.jsx
@@ -20,6 +20,7 @@ export function Ticker({ maxItems = 20, onStateChange, children }) {
   const tickerItemsRef = useRef()
   const tickerScrollRef = useRef()
   const controlsRef = useRef()
+  const topOfTickerAnchor = useRef()
 
   const [hideButtons, setHideButtons] = useState(false)
 
@@ -43,6 +44,9 @@ export function Ticker({ maxItems = 20, onStateChange, children }) {
   }, [childArray])
 
   function toggleExpandedState() {
+    if (expanded) {
+      topOfTickerAnchor.current.scrollIntoView({ behavior: "smooth" }) //scroll user up to top of ticker when closing at mobile
+    }
     setExpanded((expanded) => {
       const newState = !expanded
       if (onStateChange) onStateChange({ expanded: newState })
@@ -51,30 +55,33 @@ export function Ticker({ maxItems = 20, onStateChange, children }) {
   }
 
   return (
-    <div ref={tickerRef} className={styles.ticker} style={`--ticker-offset: ${offsetWidth}px;`} data-expanded={expanded}>
-      <div ref={tickerItemsRef} className={styles.tickerItems}>
-        <div ref={tickerScrollRef} className={styles.tickerScroll}>
-          {childArray.map((child, index) => (
-            <div className={styles.tickerItem} key={index}>
-              {child}
-            </div>
-          ))}
+    <>
+      <div id="top-of-ticker-anchor" ref={topOfTickerAnchor} />
+      <div ref={tickerRef} className={styles.ticker} style={`--ticker-offset: ${offsetWidth}px;`} data-expanded={expanded}>
+        <div ref={tickerItemsRef} className={styles.tickerItems}>
+          <div ref={tickerScrollRef} className={styles.tickerScroll}>
+            {childArray.map((child, index) => (
+              <div className={styles.tickerItem} key={index}>
+                {child}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div ref={controlsRef} className={styles.controls} style={hideButtons && { display: "none" }}>
+          <div className={styles.gradient}>
+            <Gradient />
+          </div>
+          <div className={styles.buttons}>
+            <ArrowButton onClick={() => setPageIndex((d) => d + 1)} disabled={pageIndex >= numberOfPages - 1} />
+            <ArrowButton direction="left" onClick={() => setPageIndex((d) => d - 1)} disabled={pageIndex <= 0} />
+          </div>
+          <div className={styles.button}>
+            <Button type="regular" styles={{ buttonInner: styles.buttonInner }} onClick={toggleExpandedState}>
+              {expanded ? "Show fewer" : `Show ${maxItems} most recent`}
+            </Button>
+          </div>
         </div>
       </div>
-      <div ref={controlsRef} className={styles.controls} style={hideButtons && { display: "none" }}>
-        <div className={styles.gradient}>
-          <Gradient />
-        </div>
-        <div className={styles.buttons}>
-          <ArrowButton onClick={() => setPageIndex((d) => d + 1)} disabled={pageIndex >= numberOfPages - 1} />
-          <ArrowButton direction="left" onClick={() => setPageIndex((d) => d - 1)} disabled={pageIndex <= 0} />
-        </div>
-        <div className={styles.button}>
-          <Button type="regular" styles={{ buttonInner: styles.buttonInner }} onClick={toggleExpandedState}>
-            {expanded ? "Show fewer" : `Show ${maxItems} most recent`}
-          </Button>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
