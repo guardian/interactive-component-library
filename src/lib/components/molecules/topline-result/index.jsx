19f568339781c1df64d0ccf8576d0dd6daf123a7
Antonio Voce <antonio.voce@guardian.co.uk>
2024-08-02 11:47:11 +0100

    add more mouse events to topline result

--- src/lib/components/molecules/topline-result/index.jsx
+++ src/lib/components/molecules/topline-result/index.jsx
@@ -4,12 +4,30 @@ import { forwardRef } from "preact/compat"
 import defaultStyles from "./style.module.scss"
 
 export const ToplineResult = forwardRef(
-  ({ name, secondaryName, mainNumber, mainNumberSuffix, secondaryNumber, styles, displayRow = false, abbreviation, onMouseOver, onInfoPress, showInfoButton = false, infoButtonRef }, ref) => {
+  (
+    {
+      name,
+      secondaryName,
+      mainNumber,
+      mainNumberSuffix,
+      secondaryNumber,
+      styles,
+      displayRow = false,
+      abbreviation,
+      onMouseOver,
+      onClick,
+      onMouseOut,
+      onInfoPress,
+      showInfoButton = false,
+      infoButtonRef,
+    },
+    ref,
+  ) => {
     styles = mergeStyles({ ...defaultStyles }, styles)
     const displayStyle = displayRow ? styles.displayRow : styles.displayColumn
 
     return (
-      <div class={styles.toplineResult} onMouseOver={onMouseOver} ref={ref}>
+      <div class={styles.toplineResult} onMouseOver={onMouseOver} onClick={onClick} onMouseOut={onMouseOut} ref={ref}>
         <div class={styles.topRow}>
           <span class={`${styles.primaryname} before-color--${abbreviation}`}>{name}</span>
           {showInfoButton && (
