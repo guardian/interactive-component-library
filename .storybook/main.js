12e9cdbf109ecd7fd71ead57cc0083be32923fe6
Edward Gargan <edwgargan@gmail.com>
2024-08-05 12:25:49 +0100

    feat: deploy storybook to GitHub pages; add guardian logo (#218)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,16 @@
+import { withoutVitePlugins } from "@storybook/builder-vite"
+
+/** @type {import('@storybook/preact-vite').StorybookConfig} */
 const config = {
   core: {
     disableTelemetry: true,
   },
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "./Introduction.mdx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  staticDirs: ["./assets"],
   addons: [
     "@storybook/addon-controls",
     "@storybook/addon-links",
@@ -28,6 +36,17 @@ const config = {
   docs: {
     autodocs: "tag",
     defaultName: "Documentation",
+  },
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      // Make sure to bundle all of our peer dependencies for the Storybook build,
+      // we need preact, d3-geo etc in the bundle!
+      plugins: await withoutVitePlugins(config.plugins, ["peer-deps-external"]),
+      // Vite needs to know what the 'root' path is for our site, which for out GitHub pages
+      // deployment, is our repo name (in https://guardian.github.io/interactive-component-library)
+      base: "/interactive-component-library/",
+    }
   },
 }
 export default config
