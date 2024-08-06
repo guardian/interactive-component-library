c8b7dbfcd68b8f2a1bcc297bc44cff68e311327d
Niels de Hoog <njdehoog@gmail.com>
2024-08-06 16:04:18 +0100

    Remove incorrect jsdoc comment

--- .storybook/blocks/Hint.jsx
+++ .storybook/blocks/Hint.jsx
@@ -1,11 +1,5 @@
 import styles from "./hint.module.css"
 
-/**
- * A block that displays the story name or title from the of prop
- * - if a story reference is passed, it renders the story name
- * - if a meta reference is passed, it renders the stories' title
- * - if nothing is passed, it defaults to the primary story
- */
 export const Hint = ({ icon = "💡", text }) => {
   return (
     <div className={styles.container}>
