c350172720eb4e13f545bc20017c1f25146e1406
Niels de Hoog <njdehoog@gmail.com>
2024-08-07 11:42:10 +0100

    Add US and identity projections

--- src/lib/components/molecules/canvas-map/lib/projection/index.js
+++ src/lib/components/molecules/canvas-map/lib/projection/index.js
@@ -1,11 +1,17 @@
-import { geoAlbers, geoMercator } from "d3-geo"
-import { geoAlbersUk } from "d3-composite-projections"
+import { geoAlbers, geoMercator, geoIdentity, geoAlbersUsa } from "d3-geo"
+import {
+  geoAlbersUk,
+  geoAlbersUsa as geoAlbersUSComposite,
+} from "d3-composite-projections"
 
 export const Projection = {
+  geoIdentity: geoIdentity(),
+  geoMercator: geoMercator(),
+  geoAlbersUS: geoAlbersUsa().scale(1300).translate([487.5, 305]),
+  geoAlbersUSComposite: geoAlbersUSComposite(),
   geoAlbersUKComposite: geoAlbersUk(),
   geoAlbersEngland: geoAlbers()
     .center([0, 52.7])
     .rotate([1.1743, 0])
     .parallels([50, 54]),
-  geoMercator: geoMercator(),
 }
