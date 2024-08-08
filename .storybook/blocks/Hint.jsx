601a441dc0b091060468c6418a2b94cfd30fb795
Niels de Hoog <njdehoog@gmail.com>
2024-08-08 10:54:24 +0100

    Update docs styling

--- .storybook/blocks/Hint.jsx
+++ .storybook/blocks/Hint.jsx
@@ -1,10 +1,11 @@
+import { compiler } from "markdown-to-jsx"
 import styles from "./hint.module.css"
 
 export const Hint = ({ icon = "💡", text }) => {
   return (
     <div className={styles.container}>
       <span className={styles.icon}>{icon}</span>
-      <p className={styles.paragraph}>{text}</p>
+      <p className={styles.paragraph}>{compiler(text)}</p>
     </div>
   )
 }
