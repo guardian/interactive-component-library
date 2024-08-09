6ac10517e62efc247e4df987500cbdae9817a7cc
Niels de Hoog <njdehoog@gmail.com>
2024-08-09 11:00:37 +0100

    Clean up projections

--- src/lib/components/molecules/canvas-map/lib/projection/index.js
+++ src/lib/components/molecules/canvas-map/lib/projection/index.js
@@ -1,14 +1,10 @@
 import { geoAlbers, geoMercator, geoIdentity, geoAlbersUsa } from "d3-geo"
-import {
-  geoAlbersUk,
-  geoAlbersUsa as geoAlbersUSComposite,
-} from "d3-composite-projections"
+import { geoAlbersUk } from "d3-composite-projections"
 
 export const Projection = {
   geoIdentity: geoIdentity(),
   geoMercator: geoMercator(),
   geoAlbersUS: geoAlbersUsa().scale(1300).translate([487.5, 305]),
-  geoAlbersUSComposite: geoAlbersUSComposite(),
   geoAlbersUKComposite: geoAlbersUk(),
   geoAlbersEngland: geoAlbers()
     .center([0, 52.7])
