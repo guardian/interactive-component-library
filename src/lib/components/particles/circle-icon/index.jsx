bd7e46d59b873de28b06883eab571ed59ad1dbdf
Anna Leach <anna.leach@guardian.co.uk>
2024-08-12 16:28:39 +0100

    CircleIcon: use padding var

--- src/lib/components/particles/circle-icon/index.jsx
+++ src/lib/components/particles/circle-icon/index.jsx
@@ -5,22 +5,23 @@ export const CircleIcon = ({ color, pulse = false, diameter = 11, styles }) => {
   styles = mergeStyles(defaultStyles, styles)
 
   let radius = diameter / 2
+  let padding = 2
 
   return (
     <svg
       style={styles.svg}
       fill="none"
-      height={diameter + 1}
-      viewBox={`0 0 ${diameter + 2} ${diameter + 2}`}
-      width={diameter + 1}
+      height={diameter + padding}
+      viewBox={`0 0 ${diameter + padding} ${diameter + padding}`}
+      width={diameter + padding}
       xmlns="http://www.w3.org/2000/svg"
     >
       <circle
         className={[styles.circle, pulse && styles.pulse].join(" ")}
         style={{ fill: color }}
         r={radius}
-        cx={radius + 1}
-        cy={radius + 1}
+        cx={radius + padding / 2}
+        cy={radius + padding / 2}
       />
     </svg>
   )
