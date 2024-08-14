0f5a4518811187ee4e8bb85f9e26eedddad75107
Niels de Hoog <njdehoog@gmail.com>
2024-08-14 15:13:31 +0100

    Add JSDoc types to Tooltip component

--- src/lib/components/molecules/tooltip/index.jsx
+++ src/lib/components/molecules/tooltip/index.jsx
@@ -4,6 +4,36 @@ import { rectsIntersect } from "$shared/helpers/geometry"
 import { mergeStyles } from "$styles/helpers/mergeStyles"
 import defaultStyles from "./style.module.css"
 
+/**
+ * @typedef {Object} Point
+ * @property {number} x
+ * @property {number} y
+ */
+
+/**
+ * @typedef {Object} Rect
+ * @property {number} x
+ * @property {number} y
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+ * @typedef {Object} TooltipProps
+ * @property {Element} for - The element to which the tooltip should be positioned.
+ * @property {Point} [positionInTarget] - The position of the tooltip within the target element.
+ * @property {Rect} [touchRect] - The position of the touch event that triggered the tooltip.
+ * @property {boolean} [show] - Whether the tooltip should be shown or not.
+ * @property {Object} [styles] - Additional styles to be applied to the tooltip.
+ * @property {React.ReactNode} children - The content to be displayed inside the tooltip.
+ */
+
+/**
+ * A tooltip component that displays content at a specific position relative to a target element.
+ *
+ * @param {TooltipProps} props
+ * @returns {JSX.Element}
+ */
 export function Tooltip({
   for: targetElement,
   touchRect,
