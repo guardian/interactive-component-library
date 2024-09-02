be07a1dada7b7ec398e758d8311ceb07c44f6626
egargan <ed.gargan@guardian.co.uk>
2024-09-02 18:44:53 +0100

    fix: use new Extent class in Point
    Looks like we forgot to update this Point class along with the others
    in the recent preprojected geography fixes (c07c124)

--- src/lib/components/molecules/canvas-map/lib/geometry/Point.js
+++ src/lib/components/molecules/canvas-map/lib/geometry/Point.js
@@ -1,9 +1,15 @@
+import { Extent } from "../util"
 import { Geometry } from "./Geometry"
 
 export class Point extends Geometry {
   constructor({ type = "Point", coordinates }) {
     super({ type, extent: null, coordinates })
-    this.extent = [...coordinates, ...coordinates]
+    this.extent = new Extent(
+      coordinates[0],
+      coordinates[1],
+      coordinates[0],
+      coordinates[1],
+    )
   }
 
   _getProjected(projection) {
