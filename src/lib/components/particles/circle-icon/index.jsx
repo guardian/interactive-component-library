89394785f92ef44b9f0804b34f29cf6f7c01ab95
egargan <ed.gargan@guardian.co.uk>
2024-09-11 15:07:07 +0100

    feat: add splitColor prop to CircleIcon

--- src/lib/components/particles/circle-icon/index.jsx
+++ src/lib/components/particles/circle-icon/index.jsx
@@ -1,7 +1,21 @@
 import defaultStyles from "./style.module.css"
 import { mergeStyles } from "$styles/helpers/mergeStyles"
 
-export const CircleIcon = ({ color, pulse = false, diameter = 11, styles }) => {
+/**
+ * @typedef {Object} CircleIconProps
+ * @property {string} color
+ * @property {boolean} [pulse]
+ * @property {number} [diameter=11]
+ * @property {string} [splitColor]
+ * @property {Object} [styles]
+ */
+export const CircleIcon = ({
+  color,
+  pulse = false,
+  diameter = 11,
+  styles,
+  splitColor,
+}) => {
   styles = mergeStyles(defaultStyles, styles)
 
   let radius = diameter / 2
@@ -23,6 +37,13 @@ export const CircleIcon = ({ color, pulse = false, diameter = 11, styles }) => {
         cx={radius + padding / 2}
         cy={radius + padding / 2}
       />
+      {splitColor && (
+        <path
+          d={`M ${radius + padding / 2} ${padding / 2} A ${radius} ${radius} 0 0 1 ${radius + padding / 2} ${diameter + padding / 2} L ${radius + padding / 2} ${radius + padding / 2} Z`}
+          fill={splitColor}
+          transform={`rotate(45 ${radius + padding / 2} ${radius + padding / 2})`}
+        />
+      )}
     </svg>
   )
 }
